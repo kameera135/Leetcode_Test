1db0ec4d44fe6a7b9682c504f87e85bbfb82b7c5
kameera135 <kameera135@users.noreply.github.com>
2024-11-21 16:32:11 +0530

    Add main files to repo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Leetcode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Solution solution = new Solution();
+
+        int[] result = solution.TwoSums([3, 3], 6);
+
+        Console.WriteLine(result);
+
+    }
+}
+
---
+++ Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class Solution
+    {
+        public int[] TwoSums(int[] nums, int target)
+        {
+            int[] result = new int[nums.Length];
+
+            for(int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i] + nums[i + 1] == target)
+                {
+                    Console.WriteLine(nums.Length);
+                }
+            }
+
+            return result;
+        }
+    }
+}
